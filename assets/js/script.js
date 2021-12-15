32dc234e64bf9c0c354ef6e4d245d70c96267b0c
Christina Bohn <christinajbohn@gmail.com>
2021-12-15 12:04:35 -0800

    Writing functions to append weather data to page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,39 +3,74 @@
 // Fetch One Call weather data (units = imperial)
 // Exclude minutely, hourly
 
-var cityName;
+var searchBtn = document.querySelector('#button-addon2');
+var apiKey = '130e45b8035df0b2f3d2389f4fb66852'
+$(document).ready(function() {
 
 function geoData (cityName) {
-
-  var url = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}&units=imperial';
-
-  fetch( url )
+  // event.preventDefault();
+  var cityName = document.querySelector('#cityInput').value;
+  var coordUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+  console.log(cityName)
+  fetch( coordUrl )
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        console.log( data );
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+        console.log( lat, lon );
 
-        oneCall( lat,lon );
+        oneCall( lat,lon, cityName );
     });
 
 }
 
-function oneCall(lat, lon) {
-
-  var url = '';
-
+function oneCall(lat, lon, cityName) {
+  var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
   fetch( url )
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-        appendData( data );
+      $('#todaysWeather').empty();
+
+      var currentDate = moment(data.current.dt, 'X').format('L');
+      var cardToday = $('<div>').addClass('card').css('width', '18rem');
+      var cardHeader = $('<div>').addClass('card-header').text(cityName + ' ' + currentDate);
+      var weatherList = $('<ul>').addClass('list-group list-group-flush');
+      var temp = $('<li>').addClass('list-group-item').text('Temperature: ' + data.current.temp + 'ºF');
+      var humid = $('<li>').addClass('list-group-item').text('Humidity: ' + data.current.humidity + '%');
+      var wind = $('<li>').addClass('list-group-item').text('Wind Speed: ' + data.current.wind_speed + 'MPH');
+      var uvi = $('<li>').addClass('list-group-item').text('UV Index: ' + data.current.uvi );
+
+      weatherList.append(temp, humid, wind, uvi);
+      cardToday.append(cardHeader, weatherList);
+      $('#todaysWeather').append(cardToday)
+      // console.log(currentDate)
+
+      for(let i = 1; i<data.daily.length - 2; i++) {
+       
+        var cardForecast = $('<div>').addClass('card').css('width', '18rem');
+
+        // append to forecast div
+        $('#forecast').append(cardForecast)
+      }
+      console.log(data);
     });
 
 }
 
+searchBtn.addEventListener('click',function() {
+  event.preventDefault();
+  // console.log(cityName)
+  geoData()
+} );
+})
 
+
+
+// moment(data.date, 'X').format('L')
 
 // Print weather data to cards
         // From <form> listen to "submit"
@@ -44,16 +79,17 @@ function oneCall(lat, lon) {
         // From <button> listen to "click"
             // Get the city from the button's data attribute
 
-function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-    for (var i = 0; i < data.length; i++) {
-      var div = document.createElement("div");
-      div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-      mainContainer.appendChild(div);
-    }
-  }
 
-  // moment(data.date, 'X').format('L')
+
+// function appendData(data) {
+//     var mainContainer = document.getElementById("myData");
+//     for (var i = 0; i < data.length; i++) {
+//       var div = document.createElement("div");
+//       div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+//       mainContainer.appendChild(div);
+//     }
+//   }
+
 //https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}&units=imperial
 
 
@@ -62,39 +98,39 @@ function appendData(data) {
 
 //  BEGIN FETCH DEMO -----
 
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+  // var tableBody = document.getElementById('repo-table');
+  // var fetchButton = document.getElementById('fetch-button');
 
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
+  // function getApi() {
+  //   // fetch request gets a list of all the repos for the node.js organization
+  //   var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      for (var i = 0; i < data.length; i++) {
-        // Creating elements, tablerow, tabledata, and anchor
-        var createTableRow = document.createElement('tr');
-        var tableData = document.createElement('td');
-        var link = document.createElement('a');
+  //   fetch(requestUrl)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data)
+  //       //Loop over the data to generate a table, each table row will have a link to the repo url
+  //       for (var i = 0; i < data.length; i++) {
+  //         // Creating elements, tablerow, tabledata, and anchor
+  //         var createTableRow = document.createElement('tr');
+  //         var tableData = document.createElement('td');
+  //         var link = document.createElement('a');
 
-        // Setting the text of link and the href of the link
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
+  //         // Setting the text of link and the href of the link
+  //         link.textContent = data[i].html_url;
+  //         link.href = data[i].html_url;
 
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        tableData.appendChild(link);
-        createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
-      }
-    });
-}
+  //         // Appending the link to the tabledata and then appending the tabledata to the tablerow
+  //         // The tablerow then gets appended to the tablebody
+  //         tableData.appendChild(link);
+  //         createTableRow.appendChild(tableData);
+  //         tableBody.appendChild(createTableRow);
+  //       }
+  //     });
+  // }
 
-fetchButton.addEventListener('click', getApi);
+  // fetchButton.addEventListener('click', getApi);
 
 // END FETCH DEMO -----
